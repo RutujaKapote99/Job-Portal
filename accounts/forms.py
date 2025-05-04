3cddccbe076b9f292919d1baa85169587b4d9107
RutujaKapote99 <rutuja.kapotee@gmail.com>
2025-05-04 11:15:34 +0530

    Changes added for JOb Portal

--- accounts/forms.py
+++ accounts/forms.py
@@ -4,25 +4,25 @@ from django.core.exceptions import ValidationError
 
 
 # Remove the import of Profile at the top to avoid circular import
-# from .models import Profile
+# from .models import Profile 
 
 class RegisterForm(forms.ModelForm):
-    user_type = forms.ChoiceField(choices=[('applicant', 'Employer'), ('recruiter', 'Job Seeker')])
-
+    user_type = forms.ChoiceField(choices=[('recruiter', 'Employer'), ('applicant', 'Job Seeker')])
+    password = forms.CharField(widget=forms.PasswordInput)
     class Meta:
         model = User
         fields = ['username', 'password', 'email']
 
     def save(self, commit=True):
         user = super(RegisterForm, self).save(commit=False)
+        user.set_password(self.cleaned_data['password'])  # Hash the password
         if commit:
             user.save()
-        # Lazy import Profile here to avoid circular import
-        from .models import Profile
-        Profile.objects.create(
-            user=user,
-            user_type=self.cleaned_data.get('user_type')
-        )
+            from .models import Profile
+            Profile.objects.create(
+                user=user,
+                user_type=self.cleaned_data.get('user_type')
+            )
         return user
 
 class LoginForm(forms.Form):
